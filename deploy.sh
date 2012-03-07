402494998afaa70de5b72a05b422e2521c788bcd
Ryan Pavlik <rpavlik@iastate.edu>
2012-03-07 14:22:21 -0600

    Add a deploy script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DEST=/opt/vrpn-deploy
+AUTOUSER=vrpnrestart
+
+here=$( pwd )
+tdir=$( mktemp -d )
+trap 'return_here' EXIT
+return_here () {
+    cd "$here"
+    [ -d "$tdir" ] && rm -rf "$tdir"
+}
+
+
+cd ${tdir} && \
+	cmake ${here} -DCMAKE_INSTALL_PREFIX=${DEST} \
+	&& make \
+	&& make install \
+	&& cp ${DEST}/upstart/* /etc/init/ \
+	&& cp ${DEST}/ssh/authorized_keys ~${AUTOUSER}/.ssh/authorized_keys
